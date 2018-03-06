6fcfc5350d5ff6d2e8dbcbfa641f6114859792a9
NicholasDomitio <nicholas.domitio@gmail.com>
2018-03-06 09:39:33 -0500

    first

---
+++ March5/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="March5.MvcApplication" Language="C#" %>
---
+++ March5/Models/SuggestionModel.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace March5.Models
+{
+    public class SuggestionModel
+    {
+        [Key]
+        public int RecordNum { get; set; }
+
+
+        public string Movie { get; set; }
+        public string MovieStar { get; set; }
+        public int ReviewStars { get; set; }
+    }
+}
